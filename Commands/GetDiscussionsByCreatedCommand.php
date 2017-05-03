b93470913f099770c5af18fec2bb13d1830530af
T3RAN13 <T3RAN13@bitbucket.org>
2017-05-03 22:05:24 +0300

    get_discussions_by_created update

--- Commands/GetDiscussionsByCreatedCommand.php
+++ Commands/GetDiscussionsByCreatedCommand.php
@@ -8,6 +8,22 @@ class GetDiscussionsByCreatedCommand extends CommandAbstract
 {
     protected $method = 'get_discussions_by_created';
     protected $requiredParams = [
-        0 => ['tag', 'limit']
+        ConnectorInterface::PLATFORM_GOLOS => [
+            0 => [
+                'select_authors', //'author',
+                'select_tags', //'author',
+                'limit', //'limit'
+//          'start_author', //'start_author' for pagination,
+//          'start_permlink', //'start_permlink' for pagination,
+            ]
+        ],
+        ConnectorInterface::PLATFORM_STEEMIT => [
+            0 => [
+                'tag', //'author',
+                'limit', //'limit'
+//          'start_author', //'start_author' for pagination,
+//          'start_permlink', //'start_permlink' for pagination,
+            ]
+        ]
     ];
 }
