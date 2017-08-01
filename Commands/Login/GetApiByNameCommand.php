ac9d07e7bfe79689d200ff7ff977a169ee60c8f0
t3ran13 <t262980102>
2017-08-01 10:39:23 +0300

    - update GetApiByNameCommand.php

--- Commands/Login/GetApiByNameCommand.php
+++ Commands/Login/GetApiByNameCommand.php
@@ -6,7 +6,7 @@ namespace GrapheneNodeClient\Commands\Login;
 
 class GetApiByNameCommand extends CommandAbstract
 {
-    protected $method         = 'get_api_by_name';
+    protected $method       = 'get_api_by_name';
     protected $queryDataMap = [
         '0' => ['string'], //'api_name',for example follow_api, database_api, login_api and ect.
     ];
