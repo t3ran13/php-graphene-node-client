65ad7c18cc9602f712460d09a73a5bb410281f7c
t3ran13 <t262980102>
2017-11-26 00:35:51 +0300

    - partly PrivateKeyFromWif - signTransaction

--- Tools/Auth.php
+++ Tools/Auth.php
@@ -6,6 +6,7 @@ namespace GrapheneNodeClient\Tools;
 use GrapheneNodeClient\Commands\CommandQueryDataInterface;
 use GrapheneNodeClient\Tools\ChainOperations\ChainOperations;
 use GrapheneNodeClient\Tools\ChainOperations\OperationSerializer;
+use StephenHill\Base58;
 use TrafficCophp\ByteBuffer\Buffer;
 
 class Auth
@@ -27,7 +28,7 @@ class Auth
     }
 
 
-    public static function signTransaction($chainName, CommandQueryDataInterface $trx)
+    public static function signTransaction($chainName, CommandQueryDataInterface $trx, $privKyes)
     {
 //        var signatures = [];
 //        if (trx.signatures) {
@@ -53,22 +54,94 @@ class Auth
 
         echo '<pre>' . print_r($trxParams, true) . '<pre>';  //FIXME delete it
 
-//        $cid = hex2bin(self::getChainId($chainName));
-        $cid = self::getChainId($chainName);
+        //serialize only transaction data
+        $serBuffer = OperationSerializer::serializeTransaction($trxParams, new Buffer());
 
-        //only transaction data
-        $buf = OperationSerializer::serializeTransaction($trxParams, new Buffer());
-
-        //only operations data
+        //serialize only operations data
         foreach ($trxParams[0]['operations'] as $operation) {
             $opData = $operation[1];
-            OperationSerializer::serializeOperation($operation[0], $opData, $buf);
+            OperationSerializer::serializeOperation($operation[0], $opData, $serBuffer);
         }
-        echo "\n" . print_r($buf->getBuffer('H', 0, $buf->getCurrentOffset()), true) . '<pre>'; die; //FIXME delete it
+//        echo "\n" . print_r($serBuffer->getBuffer('H', 0, $serBuffer->getCurrentOffset()), true) . '<pre>'; die; //FIXME delete it
+
+
+//        $cid = hex2bin(self::getChainId($chainName));
+        $cidBuffer = new Buffer();
+        $cidBuffer->writeVHexStringBE(self::getChainId($chainName));
+        $sigBuffer = $cidBuffer->concat($cidBuffer, $serBuffer);
+//        echo '<pre>' . print_r($sigBuffer, true) . '<pre>'; die; //FIXME delete it
+//        echo "\n" . print_r($sigBuffer->getBuffer('H', 0, $sigBuffer->getCurrentOffset()), true) . '<pre>'; die; //FIXME delete it
+
+        foreach ($privKyes as $keyName => $key) {
+            $privateKey = self::PrivateKeyFromWif($key);
+
+            $context = secp256k1_context_create(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY);
+
+            $msg32 = hash('sha256', $sigBuffer->getBuffer('H', 0, $sigBuffer->getCurrentOffset()), true);
+            $privateKey = pack("H*", $privateKey);
+            /** @var resource $signature */
+            $signature = '';
+
+            if (secp256k1_ecdsa_sign($context, $signature, $msg32, $privateKey) != 1) {
+                throw new \Exception("Failed to create signature");
+            }
+
+            $serialized = '';
+            secp256k1_ecdsa_signature_serialize_der($context, $serialized, $signature);
+            echo sprintf("Produced signature: %s \n", bin2hex($serialized));
+            echo '<pre>' . print_r(123, true) . '<pre>'; //FIXME delete it
+        }
+
+        die;
 
 
 
 
         return $out;
+    }
+
+    public static function PrivateKeyFromWif($privWif) {
+//        var private_wif = new Buffer(base58.decode(_private_wif));
+//        var version = private_wif.readUInt8(0);
+//        assert.equal(0x80, version, `Expected version ${0x80}, instead got ${version}`);
+//        // checksum includes the version
+//        var private_key = private_wif.slice(0, -4);
+//        var checksum = private_wif.slice(-4);
+//        var new_checksum = hash.sha256(private_key);
+//        new_checksum = hash.sha256(new_checksum);
+//        new_checksum = new_checksum.slice(0, 4);
+//        if (checksum.toString() !== new_checksum.toString())
+//            throw new Error('Invalid WIF key (checksum miss-match)')
+//
+//        private_key = private_key.slice(1);
+//return PrivateKey.fromBuffer(private_key);
+//echo '<pre>' . print_r($privWif, true) . '<pre>'; die; //FIXME delete it
+
+        //checking wif version
+        $base58 = new Base58();
+        $wifBuffer = new Buffer();
+        $wifBuffer->write($base58->decode($privWif));
+        $version = $wifBuffer->readInt8(0);
+        if ($wifBuffer->readInt8(0) !== 128) {
+            //        assert.equal(0x80, version, `Expected version ${0x80}, instead got ${version}`);
+            throw new \Exception('Expected version 128, instead got ' . $version);
+        }
+
+        //checking WIF checksum
+        $private_key = $wifBuffer->read(0, $wifBuffer->length() - 4);
+        $checksum = $wifBuffer->read($wifBuffer->length() - 4, 4);
+        $new_checksum = hash('sha256', $private_key, true);
+        $new_checksum = hash('sha256', $new_checksum, true);
+        $new_checksum = substr($new_checksum, 0, 4);
+        if ($new_checksum !== $checksum) {
+            throw new \Exception('Invalid WIF key (checksum miss-match)');
+        }
+
+        //getting private_key
+        $private_key = substr($private_key, 1);
+        echo '<pre>' . print_r(strlen($private_key), true) . '<pre>'; die; //FIXME delete it
+        echo "\n" . print_r($wifBuffer->getBuffer('H', 0, $wifBuffer->getCurrentOffset()), true) . '<pre>'; die; //FIXME delete it
+
+        return $answer;
     }
 }
