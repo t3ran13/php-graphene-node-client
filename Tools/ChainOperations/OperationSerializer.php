7bef094cad4407200bc61259cb7c67b1059fe75b
t3ran13 <teran-555@yandex.ru>
2018-04-02 21:15:06 +0300

    fix critical broadcast bug #10 #7
    fix error: 3030000 tx_missing_posting_auth: missing required posting authority
    it was serializer string bug

--- Tools/ChainOperations/OperationSerializer.php
+++ Tools/ChainOperations/OperationSerializer.php
@@ -15,7 +15,6 @@ class OperationSerializer
             'voter'    => self::TYPE_STRING,
             'author'   => self::TYPE_STRING,
             'permlink' => self::TYPE_STRING,
-            'permlink' => self::TYPE_STRING,
             'weight'   => self::TYPE_INT16
         ],
         ChainOperations::OPERATION_COMMENT  => [
@@ -99,7 +98,19 @@ class OperationSerializer
     public static function serializeType($type, $value, $byteBuffer)
     {
         if ($type === self::TYPE_STRING) {
-            $byteBuffer->writeInt8(strlen($value));
+            //Writes a UTF8 encoded string prefixed 32bit base 128 variable-length integer.
+            $strLength = strlen($value);
+            if ($strLength < 128) {
+                $byteBuffer->writeInt8($strLength);
+            } elseif ($strLength < 256) {
+                $strLength = round($strLength / 128, 0) * 256
+                    + ($strLength - (round($strLength / 128, 0) - 1) * 128);
+                $byteBuffer->writeInt16LE($strLength);
+            } else {
+                $strLength = round($strLength / 128, 0) * 256
+                    + ($strLength - round($strLength / 128, 0) * 128);
+                $byteBuffer->writeInt16LE($strLength);
+            }
             $byteBuffer->writeVStringLE($value);
         } elseif ($type === self::TYPE_INT16) {
             $byteBuffer->writeInt16LE($value);
