bec6dda96dc29bd999fa72cb4b813b2cc6c645db
t3ran13 <teran-555@yandex.ru>
2018-06-06 10:08:17 +0300

    code refactoring

--- Tools/ChainOperations/ChainOperations.php
+++ Tools/ChainOperations/ChainOperations.php
@@ -5,15 +5,15 @@ namespace GrapheneNodeClient\Tools\ChainOperations;
 
 class ChainOperations
 {
-    const OPERATION_VOTE = 'vote';
-    const OPERATION_COMMENT = 'comment';
-    const OPERATION_TRANSFER = 'transfer';
+    const OPERATION_VOTE        = 'vote';
+    const OPERATION_COMMENT     = 'comment';
+    const OPERATION_TRANSFER    = 'transfer';
     const OPERATION_CUSTOM_JSON = 'custom_json';
 
     const OPERATIONS_IDS = [
-        self::OPERATION_VOTE => 0,
-        self::OPERATION_COMMENT => 1,
-        self::OPERATION_TRANSFER => 2,
+        self::OPERATION_VOTE        => 0,
+        self::OPERATION_COMMENT     => 1,
+        self::OPERATION_TRANSFER    => 2,
         self::OPERATION_CUSTOM_JSON => 18
     ];
 
