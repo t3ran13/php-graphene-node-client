9f4762eb64e7720c5e518c88232b5e5a182cb01e
t3ran13 <teran-555@yandex.ru>
2018-10-15 00:10:37 +0300

    add new operations types

--- Tools/ChainOperations/OperationSerializer.php
+++ Tools/ChainOperations/OperationSerializer.php
@@ -6,10 +6,13 @@ use t3ran13\ByteBuffer\ByteBuffer;
 
 class OperationSerializer
 {
-    const TYPE_SET_STRING = 'set_string';
-    const TYPE_STRING     = 'string';
-    const TYPE_INT16      = 'int16';
-    const TYPE_ASSET      = 'asset';
+    const TYPE_SET_EXTENSIONS   = 'set_extensions';
+    const TYPE_SET_BENEFICIARIES= 'set_beneficiaries';
+    const TYPE_BENEFICIARY      = 'set_beneficiary';
+    const TYPE_SET_STRING       = 'set_string';
+    const TYPE_STRING           = 'string';
+    const TYPE_INT16            = 'int16';
+    const TYPE_ASSET            = 'asset';
 
     const OPERATIONS_FIELDS_TYPES = [
         ChainOperations::OPERATION_VOTE        => [
@@ -18,14 +21,25 @@ class OperationSerializer
             'permlink' => self::TYPE_STRING,
             'weight'   => self::TYPE_INT16
         ],
-        ChainOperations::OPERATION_COMMENT     => [
-            'parent_author'   => self::TYPE_STRING,
-            'parent_permlink' => self::TYPE_STRING,
-            'author'          => self::TYPE_STRING,
-            'permlink'        => self::TYPE_STRING,
-            'title'           => self::TYPE_STRING,
-            'body'            => self::TYPE_STRING,
-            'json_metadata'   => self::TYPE_STRING
+        ChainOperations::OPERATION_COMMENT     => [//STEEM/GOLOS
+            'parent_author'    => self::TYPE_STRING,
+            'parent_permlink'  => self::TYPE_STRING,
+            'author'           => self::TYPE_STRING,
+            'permlink'         => self::TYPE_STRING,
+            'title'            => self::TYPE_STRING,
+            'body'             => self::TYPE_STRING,
+            'json_metadata'    => self::TYPE_STRING
+        ],
+        ChainOperations::OPERATION_CONTENT     => [//only for VIZ
+            'parent_author'    => self::TYPE_STRING,
+            'parent_permlink'  => self::TYPE_STRING,
+            'author'           => self::TYPE_STRING,
+            'permlink'         => self::TYPE_STRING,
+            'title'            => self::TYPE_STRING,
+            'body'             => self::TYPE_STRING,
+            'curation_percent' => self::TYPE_INT16,
+            'json_metadata'    => self::TYPE_STRING,
+            'extensions'       => self::TYPE_SET_EXTENSIONS
         ],
         ChainOperations::OPERATION_TRANSFER    => [
             'from'   => self::TYPE_STRING,
@@ -127,7 +141,7 @@ class OperationSerializer
                 }
             }
             $byteBuffer->writeVStringLE($value);
-        } if ($type === self::TYPE_SET_STRING) {
+        } elseif ($type === self::TYPE_SET_STRING) {
             $byteBuffer->writeInt8(count($value));
             foreach ($value as $string) {
                 self::serializeType(self::TYPE_STRING, $string, $byteBuffer);
@@ -149,6 +163,22 @@ class OperationSerializer
             for ($i = 0; $i < 7 - strlen($symbol); $i++) {
                 $byteBuffer->writeInt8(0);
             }
+        } elseif ($type === self::TYPE_SET_EXTENSIONS) {
+            $byteBuffer->writeInt8(count($value));
+            foreach ($value as $extension) {
+                $byteBuffer->writeInt8($extension[0]);
+                if ($extension[0] === 0) {
+                    self::serializeType(self::TYPE_SET_BENEFICIARIES, $extension[1], $byteBuffer);
+                }
+            }
+        } elseif ($type === self::TYPE_SET_BENEFICIARIES) {
+            $byteBuffer->writeInt8(count($value));
+            foreach ($value['beneficiaries'] as $beneficiary) {
+                self::serializeType(self::TYPE_BENEFICIARY, $beneficiary, $byteBuffer);
+            }
+        } elseif ($type === self::TYPE_BENEFICIARY) {
+            self::serializeType(self::TYPE_STRING, $value['account'], $byteBuffer);
+            self::serializeType(self::TYPE_INT16, $value['weight'], $byteBuffer);
         }
 
         return $byteBuffer;
