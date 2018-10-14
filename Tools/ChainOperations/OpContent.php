18b3ac3dd90ef3138b75cdb78847acb3fa1e518b
t3ran13 <teran-555@yandex.ru>
2018-10-15 00:12:33 +0300

    add new operation content for VIZ

--- Tools/ChainOperations/OpContent.php
+++ Tools/ChainOperations/OpContent.php
@@ -8,8 +8,8 @@ use GrapheneNodeClient\Commands\CommandQueryData;
 use GrapheneNodeClient\Connectors\ConnectorInterface;
 use GrapheneNodeClient\Tools\Transaction;
 
-//STEEM/GOLOS
-class OpComment
+//ONLY VIZ
+class OpContent
 {
     /**
      * @param ConnectorInterface $connector
@@ -19,21 +19,24 @@ class OpComment
      * @param string             $title
      * @param string             $body
      * @param string             $jsonMetadata
+     * @param integer            $curationPercent 10000 as 100%
+     * @param array[]            $beneficiaries as array [['account' => 'denis-skripnik', 'weight' => 10000]], weight is max 10000 as 100%
      * @param string             $parentPermlink
      * @param string             $parentAuthor
      *
      * @return mixed
      * @throws \Exception
      */
-    public static function do(ConnectorInterface $connector, $privatePostingWif, $author, $permlink, $title, $body, $jsonMetadata, $parentPermlink, $parentAuthor = '')
+    public static function do(ConnectorInterface $connector, $privatePostingWif, $author, $permlink, $title, $body, $jsonMetadata, $curationPercent = 0, $beneficiaries = [], $parentPermlink, $parentAuthor = '')
     {
         $chainName = $connector->getPlatform();
+
         /** @var CommandQueryData $tx */
         $tx = Transaction::init($connector);
         $tx->setParamByKey(
             '0:operations:0',
             [
-                'comment',
+                ChainOperations::OPERATION_CONTENT,
                 [
                     'parent_author'   => $parentAuthor,
                     'parent_permlink' => $parentPermlink,
@@ -41,7 +44,9 @@ class OpComment
                     'permlink'        => $permlink,
                     'title'           => $title,
                     'body'            => $body,
-                    'json_metadata'   => $jsonMetadata
+                    'curation_percent'=> $curationPercent,
+                    'json_metadata'   => $jsonMetadata,
+                    'extensions'      => empty($beneficiaries) ? [] : [[0, ["beneficiaries" => $beneficiaries]]]
                 ]
             ]
         );
@@ -64,13 +69,15 @@ class OpComment
      * @param string             $title
      * @param string             $body
      * @param string             $jsonMetadata
+     * @param integer            $curationPercent 10000 as 100%
+     * @param array[]            $beneficiaries as array [['account' => 'denis-skripnik', 'weight' => 10000]], weight is max 10000 as 100%
      * @param string             $parentPermlink
      * @param string             $parentAuthor
      *
      * @return mixed
      * @throws \Exception
      */
-    public static function doSynchronous(ConnectorInterface $connector, $privatePostingWif, $author, $permlink, $title, $body, $jsonMetadata, $parentPermlink, $parentAuthor = '')
+    public static function doSynchronous(ConnectorInterface $connector, $privatePostingWif, $author, $permlink, $title, $body, $jsonMetadata, $curationPercent = 0, $beneficiaries = [], $parentPermlink, $parentAuthor = '')
     {
         $chainName = $connector->getPlatform();
         /** @var CommandQueryData $tx */
@@ -78,7 +85,7 @@ class OpComment
         $tx->setParamByKey(
             '0:operations:0',
             [
-                'comment',
+                ChainOperations::OPERATION_CONTENT,
                 [
                     'parent_author'   => $parentAuthor,
                     'parent_permlink' => $parentPermlink,
@@ -86,7 +93,9 @@ class OpComment
                     'permlink'        => $permlink,
                     'title'           => $title,
                     'body'            => $body,
-                    'json_metadata'   => $jsonMetadata
+                    'curation_percent'=> $curationPercent,
+                    'json_metadata'   => $jsonMetadata,
+                    'extensions'      => empty($beneficiaries) ? [] : [[0, ["beneficiaries" => $beneficiaries]]]
                 ]
             ]
         );
